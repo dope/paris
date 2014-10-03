eb4c56d7ee5db64efe487435da67477c8c91c32d
Joe Richardson <jr@studio-40.com>
2014-10-03 23:31:45 +0100

    added autoprefixr and compressor for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 // requirements
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var browserSync  = require('browser-sync');
+var prefix       = require('gulp-autoprefixer');
 
 // create sass task
 gulp.task('sass', function () {
     gulp.src('scss/**/*.scss')
-        .pipe(sass({includePaths: ['scss']}))
+        .pipe(sass({outputStyle: 'compressed', includePaths: ['scss']}))
+        .pipe(prefix("last 2 versions", "> 1%", "ie 8", "Android 2", "Firefox ESR"))
         .pipe(gulp.dest('css'));
 });
 
