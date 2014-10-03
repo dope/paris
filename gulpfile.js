f8e01e50d27c0461ec0b2ca37edc854c6789de93
Joe Richardson <jr@studio-40.com>
2014-10-04 00:13:56 +0100

    added media queries, filled out color, image, hr and type page

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass         = require('gulp-sass');
 var browserSync  = require('browser-sync');
 var prefix       = require('gulp-autoprefixer');
 
-// create sass task
+// create sass tasks
 gulp.task('sass', function () {
     gulp.src('scss/**/*.scss')
         .pipe(sass({outputStyle: 'compressed', includePaths: ['scss']}))
