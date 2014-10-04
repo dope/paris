55aeb7d66817a2b9e8097be2550c833a5ac42a4c
Joe Richardson <jr@studio-40.com>
2014-10-04 15:28:28 +0100

    added shit load of mixins

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulp.task('sass', function () {
     gulp.src('scss/**/*.scss')
         .pipe(sass({outputStyle: 'compressed', includePaths: ['scss']}))
         .pipe(prefix("last 2 versions", "> 1%", "ie 8", "Android 2", "Firefox ESR"))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
 });
 
 // create browser sync task
