5031e88f7ed2be1a0c7c4f89259d6b023ba77f81
Tobias <tobiaswhd@gmail.com>
2015-01-12 20:54:59 +0000

    to win a war, you gotta become war

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,20 @@ var prefix       = require('gulp-autoprefixer');
 gulp.task('sass', function () {
     gulp.src('scss/**/*.scss')
         .pipe(sass({outputStyle: 'compressed', includePaths: ['scss']}))
-        .pipe(prefix("last 2 versions", "> 1%", "ie 8", "Android 2", "Firefox ESR"))
+        .pipe(prefix('last 2 versions', '> 1%', 'ie 8', 'Android 2', 'Firefox ESR'))
         .pipe(gulp.dest('css'))
 });
 
 // create browser sync task
 gulp.task('browser-sync', function() {
-    browserSync.init(["css/*.css"], {
+    browserSync.init(['css/*.css', 'js/**/*.js', 'index.html'], {
         server: {
-            baseDir: "./"
+            baseDir: './'
         }
     });
 });
 
 // default task (just run gulp)
 gulp.task('default', ['sass', 'browser-sync'], function () {
-    gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch('scss/**/*.scss', ['sass']);
 });
---
+++ js/paris.js
@@ -0,0 +1,20 @@
+/**
+ * Paris - Modern Framework, by @dope
+ *
+ * @file - Paris API
+ * @author Tobias Harison-Denby
+ * @namespace Paris
+ */
+(function (window, document) {
+  'use strict';
+
+  var API = {};
+
+  // defaults?
+
+
+
+  // expose to global namespace
+  window.Paris = API;
+
+}(window, document));
