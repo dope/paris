7089e272dc7ca87e643a02f6b189c0c2aba4e701
Joe Richardson <hello.joer@gmail.com>
2015-10-25 14:15:51 +0000

    added gulp sass error handling

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,19 @@ var gulp         = require('gulp');
 var sass         = require('gulp-sass');
 var browserSync  = require('browser-sync');
 var prefix       = require('gulp-autoprefixer');
-var plumber      = require('gulp-plumber');
 var concat       = require('gulp-concat');
 var uglify       = require('gulp-uglify');
 var jshint       = require('gulp-jshint');
+var notify       = require('gulp-notify');
 
 // create sass tasks
 gulp.task('sass', function() {
   gulp.src('scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed', errLogToConsole: true}))
+    .pipe(sass())
+      .on('error', notify.onError(function (error) {
+         return 'An error occurred while compiling sass.\nLook in the console for details.\n' + error;
+      }))
     .pipe(prefix('last 2 versions', '> 1%', 'ie 8', 'Android 2', 'Firefox ESR'))
-    .pipe(plumber())
     .pipe(gulp.dest('css'));
 });
 
