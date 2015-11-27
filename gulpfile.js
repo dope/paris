3347d20ba0b5225902494fe6c634eb6c66c1de7b
Joe Richardson <hello.joer@gmail.com>
2015-11-27 15:12:02 +0000

    added container and centered grid

--- gulpfile.js
+++ gulpfile.js
@@ -38,8 +38,11 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('js/min/'));
 });
 
-// default task (just run gulp)
-gulp.task('default', ['sass', 'browser-sync', 'scripts'], function () {
+// build assets and start browser-sync server
+gulp.task('watch', ['sass', 'browser-sync', 'scripts'], function () {
   gulp.watch('scss/**/*.scss', ['sass']);
   gulp.watch('js/*.js', ['scripts']);
 });
+
+// default task (build assets)
+gulp.task('default', ['sass', 'scripts']);
